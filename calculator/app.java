cd3ca43351ea89a0ac4b1bedb6920fc9336d7cd1
im-allan <al.silvaquiroz@gmail.com>
2024-01-02 23:43:48 -0300

    feat: Updated java file

--- calculator/app.java
+++ calculator/app.java
@@ -1,9 +1,26 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class app {
-  
-  public static void main(String[] args) {
-    System.out.println("Hola mundo!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce el primer número:");
+        int numero1 = scanner.nextInt();
+
+        System.out.println("Introduce el segundo número:");
+        int numero2 = scanner.nextInt();
+
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        int division = numero1 / numero2;
+
+        System.out.println("La suma es: " + suma);
+        System.out.println("La resta es: " + resta);
+        System.out.println("La multiplicación es: " + multiplicacion);
+        System.out.println("La división es: " + division);
+    }
 }
 
-}
